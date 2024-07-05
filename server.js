dcacf78fab4e095d827ce9d977635ddcf37bcfd2
Alphaeu <dealphaeus@gmail.com>
2024-07-05 13:18:52 -0700

    Code corrections

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
+#!/usr/bin/node
+
 import express from 'express';
 import startServer from './libs/boot';
 import injectRoutes from './routes';
 import injectMiddlewares from './libs/middlewares';
 
-const server = express();
-
 injectMiddlewares(server);
 injectRoutes(server);
 startServer(server);
 
-export default server;
+const express = require("express");
+const router = require("./routes/index");
+
+const server = express();
+const PORT = process.env.PORT ? process.env.PORT : 5000;
+
+server.use(express.json());
+server.use(router);
+
+server.listen(PORT, () =>
+  console.log(`The server is running on port: ${PORT}`)
+);
